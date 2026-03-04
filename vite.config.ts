36d899865078ea1bb0dedc656124d2c33bed0dac
KienBanana <boom16vn@gmail.com>
2026-03-05 00:12:50 +0700

    Update feature

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
     plugins: [react(), tailwindcss()],
+    base: "/videosaver/",
     define: {
       "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
     },
