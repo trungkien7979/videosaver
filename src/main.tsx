c0886f378a73516fb6f9b3907c3057f6da97ce8c
KienBanana <boom16vn@gmail.com>
2026-03-05 09:37:53 +0700

    update 4

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 console.log("main.tsx: Root element:", rootElement);
 
 if (rootElement) {
   createRoot(rootElement).render(
-    <StrictMode>
+    <BrowserRouter>
       <App />
-    </StrictMode>,
+    </BrowserRouter>,
   );
   console.log("main.tsx: Render called");
 } else {
